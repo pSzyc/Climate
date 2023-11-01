28464d0f4f149384b2382bdc5d129becc60a6d3c
Przemek <przemek.7678@gmail.com>
2023-11-01 13:14:37 +0100

    ngrams modified

--- Classification/resources/get_data.py
+++ Classification/resources/get_data.py
@@ -5,9 +5,10 @@ def get_current_data(configuration):
     corpus = configuration.corpus
     df_eco = pd.read_csv(drive_path / corpus / "eco_result.csv", index_col=['id', 'source'], parse_dates=['date'])
     df_non_eco = pd.read_csv(drive_path / corpus / "non_eco_result.csv", index_col = ['id', 'source'], parse_dates=['date'])
+    df_rest = pd.read_csv(drive_path / corpus / "results.csv", index_col = ['id', 'source'], parse_dates=['date'])
     df_eco['label'] = 1
     df_non_eco['label'] = 0
-    return df_eco,df_non_eco
+    return df_eco, df_non_eco, df_rest
 
 def make_dataset(configuration, df_eco, df_non_eco):
     if configuration.corpus == "rzepa":
@@ -24,10 +25,3 @@ def make_dataset(configuration, df_eco, df_non_eco):
         df_sample = df_7.sample(n = 10000)
         df = pd.concat([df_eco, df_non_eco, df_sample])
     return df
-
-def get_rest_of_data(configuration):
-    if configuration.corpus == "rzepa":
-        df_rest = pd.read_csv(configuration.drive_path / configuration.corpus / "results.csv", index_col = ['id', 'source'], parse_dates=['date'])
-    else: 
-        df_rest = pd.read_csv(configuration.drive_path / configuration.corpus / "results.csv", index_col = ['id', 'source'], parse_dates=['date'])
-    return df_rest
