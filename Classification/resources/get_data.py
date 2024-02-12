0bbad0c4507811425ab68ab21738720486949f96
pszyc <przemek.7678@gmail.com>
2024-02-12 13:40:44 +0100

    xd

--- Classification/resources/get_data.py
+++ Classification/resources/get_data.py
@@ -16,10 +16,9 @@ def get_current_data(configuration):
         df_eco = df_eco.rename(columns={"author 1": 'author'})
         df_non_eco = df_non_eco.rename(columns={"author 1": 'author'})
         df_rest = df_rest.rename(columns={"author 1": 'author'})
-    drastic = corpus == 'wyborcza'
-    df_eco = validate(df_eco, drastic)
-    df_non_eco = validate(df_non_eco, drastic)
-    df_rest = validate(df_rest, drastic)
+    df_eco = validate(df_eco)
+    df_non_eco = validate(df_non_eco)
+    df_rest = validate(df_rest)
 
     return df_eco, df_non_eco, df_rest
 
@@ -34,19 +33,11 @@ def make_dataset(configuration, df_eco, df_non_eco):
         df_non_eco_rzepa = pd.read_csv(drive_path / "rzepa" / "non_eco_result.csv", index_col = ['id', 'source'], parse_dates=['date'])
         df_eco_rzepa['label'] = 1
         df_non_eco_rzepa['label'] = 0
-        if corpus in ['gpc', 'wyborcza']:
-            df_eco_rzepa = df_eco_rzepa.sample(1000)
-            df_non_eco_rzepa = df_non_eco_rzepa.sample(1000)
-            df_non_eco = df_non_eco.sample(2000)
+        df_eco_rzepa = df_eco_rzepa.sample(2000)
         df = pd.concat([df_eco, df_eco, df_non_eco, df_non_eco, df_eco_rzepa, df_non_eco_rzepa])
-    if corpus == 'wyborcza':
-        df = validate(df, drastic = True)
-    else:
-        df = validate(df)
+    df = validate(df)
     return df
 
-def validate(df, drastic = False):
-    if drastic:
-        df = df.drop_duplicates(subset=['title', 'author'])
+def validate(df):
     df = df.drop_duplicates(subset='text')
     return df
